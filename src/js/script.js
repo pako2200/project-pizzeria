de9a3ee6a62c8cd71c21ae7231aca43676008329
Pawel Kowalewski <pawelkowlw@gmail.com>
2022-07-01 03:22:31 +0200

    fix indent problem

--- src/js/script.js
+++ src/js/script.js
@@ -1,112 +1,112 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-	'use strict';
+  'use strict';
 
-	const select = {
-		templateOf: {
-			menuProduct: '#template-menu-product',
-		},
-		containerOf: {
-			menu: '#product-list',
-			cart: '#cart',
-		},
-		all: {
-			menuProducts: '#product-list > .product',
-			menuProductsActive: '#product-list > .product.active',
-			formInputs: 'input, select',
-		},
-		menuProduct: {
-			clickable: '.product__header',
-			form: '.product__order',
-			priceElem: '.product__total-price .price',
-			imageWrapper: '.product__images',
-			amountWidget: '.widget-amount',
-			cartButton: '[href="#add-to-cart"]',
-		},
-		widgets: {
-			amount: {
-				input: 'input[name="amount"]',
-				linkDecrease: 'a[href="#less"]',
-				linkIncrease: 'a[href="#more"]',
-			},
-		},
-	};
+  const select = {
+    templateOf: {
+      menuProduct: '#template-menu-product',
+    },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
+    },
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
+    },
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
 
-	const classNames = {
-		menuProduct: {
-			wrapperActive: 'active',
-			imageVisible: 'active',
-		},
-	};
+  const classNames = {
+    menuProduct: {
+      wrapperActive: 'active',
+      imageVisible: 'active',
+    },
+  };
 
-	// const settings = {
-	// 	amountWidget: {
-	// 		defaultValue: 1,
-	// 		defaultMin: 1,
-	// 		defaultMax: 9,
-	// 	}
-	// };
+  const settings = {
+    amountWidget: {
+      defaultValue: 1,
+      defaultMin: 1,
+      defaultMax: 9,
+    }
+  };
 
-	const templates = {
-		menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-	};
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  };
 
-	class Product {
-		constructor(id, data) {
-			const thisProduct = this;
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
 
-			thisProduct.id = id;
-			thisProduct.data = data;
-			thisProduct.renderInMenu();
-			thisProduct.initAccordion();
-		}
-		renderInMenu() {
-			const thisProduct = this;
-			const generatedHTML = templates.menuProduct(thisProduct.data);
-			thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-			const menuContainer = document.querySelector(select.containerOf.menu);
-			menuContainer.appendChild(thisProduct.element);
-		}
-		initAccordion() {
-			const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+    }
+    renderInMenu() {
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion() {
+      const thisProduct = this;
 
-			const clicableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-			clicableTrigger.addEventListener('click', function (event) {
-				event.preventDefault();
-				const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
-				for (let product of activeProducts) {
-					if (product != thisProduct.element) {
-						product.classList.remove('active');
-					}
-				}
-				thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-			});
+      const clicableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      clicableTrigger.addEventListener('click', function (event) {
+        event.preventDefault();
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        for (let product of activeProducts) {
+          if (product != thisProduct.element) {
+            product.classList.remove('active');
+          }
+        }
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
 
-		}
-	}
+    }
+  }
 
-	const app = {
-		initMenu() {
-			const thisApp = this;
-			for (let productData in thisApp.data.products) {
-				new Product(productData, thisApp.data.products[productData]);
-			}
-		},
-		initData() {
-			const thisApp = this;
-			thisApp.data = dataSource;
-		},
-		init: function () {
-			const thisApp = this;
-			//console.log('*** App starting ***');
-			//console.log('thisApp:', thisApp);
-			//console.log('classNames:', classNames);
-			//console.log('settings:', settings);
-			//console.log('templates:', templates);
-			thisApp.initData();
-			thisApp.initMenu();
-		},
-	};
-	app.init();
+  const app = {
+    initMenu() {
+      const thisApp = this;
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData() {
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+    init: function () {
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
+    },
+  };
+  app.init();
 }
